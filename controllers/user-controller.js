7fdad2b199d67dc666df6c60a73b0d0c63bfb576
connorshea15 <connorshea15@gmail.com>
2020-09-26 12:08:56 -0700

    create user and get all users apis are working

---
+++ controllers/user-controller.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUser(req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    }
+}
+
+module.exports = userController;
