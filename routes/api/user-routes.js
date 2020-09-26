f500f500971cb845e0607065a89af20a18cba0b9
connorshea15 <connorshea15@gmail.com>
2020-09-26 12:22:46 -0700

    User routes are all working like a charm

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
 const {
     getAllUser,
-    createUser
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser
 } = require('../../controllers/user-controller');
 
 router
     .route('/')
     .get(getAllUser)
     .post(createUser);
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
